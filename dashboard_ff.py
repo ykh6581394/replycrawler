2e1f50f2efd0a21a9440c5b4062e5c22e1ff396d
ykh <ykh6581394@hanmail.net>
2024-04-26 10:01:18 +0900

    asdfg

--- dashboard_ff.py
+++ dashboard_ff.py
@@ -16,6 +16,7 @@ warnings.filterwarnings('ignore')
 from selenium import webdriver
 from  selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.options import Options
+from selenium.webdriver.firefox.service import Service
 import time
 from bs4 import BeautifulSoup
 from webdriver_manager.firefox import GeckoDriverManager
